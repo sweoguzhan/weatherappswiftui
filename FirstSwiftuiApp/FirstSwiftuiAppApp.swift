9a3a1504c6315c45bdf69bb6cffa5bb2d7fc6612
sweoguzhan <100108189+sweoguzhan@users.noreply.github.com>
2025-02-15 18:16:58 +0300

    Initial Commit

---
+++ FirstSwiftuiApp/FirstSwiftuiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftuiAppApp.swift
+//  FirstSwiftuiApp
+//
+//  Created by Oğuzhan Çengel on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftuiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
